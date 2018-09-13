7c1d2958ad1d654eff406a1e67bb74dd424739c2
JeremiahJacobson <jeremiah.g.jacobson@gmail.com>
2018-09-13 00:18:50 -0400

    removed 'localhost' from allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(9)ynftw-+8f8o#psku2d7+wbfnxjimye7el=tbff@c$3f513+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
