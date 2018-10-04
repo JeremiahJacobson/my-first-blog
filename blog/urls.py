4465a9b18b10b8eb4c3d2c4461e784da12c43806
JeremiahJacobson <jeremiah.g.jacobson@gmail.com>
2018-10-04 17:13:52 -0400

    Added a homepage app to redirect to other apps

--- blog/urls.py
+++ blog/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     url(r'^drafts/$', views.post_draft_list, name='post_draft_list'),
     url(r'^post/(?P<pk>\d+)/publish/$', views.post_publish, name='post_publish'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    path('home_view', views.home_view, name='home_view'),
+
 ]
